be91d86dd30e0ffa7aa779d9035d9fb0feb0367f
kaijunfong <e0957867@u.nus.edu>
2025-11-01 21:35:16 +0800

    Added Firebase Auth and env variables

--- frontend/src/User_Authentication/firebase_setup.js
+++ frontend/src/User_Authentication/firebase_setup.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 
-  authDomain: 
-  projectId: 
-  storageBucket: 
-  messagingSenderId: 
-  appId: 
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -17,4 +17,3 @@ const app = initializeApp(firebaseConfig);
 export default app;
 // getting authentication and exporting it out
 export const auth = getAuth();
-
